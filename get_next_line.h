89b8b65484e5a8866415475db277bfc4d7e73f0b
Mostafa Omrane <momrane@bess-f4r2s9.clusters.42paris.fr>
2023-11-22 15:17:25 +0100

    Project set up, so lets understand static variable

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: momrane <momrane@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 15:04:12 by momrane           #+#    #+#             */
+/*   Updated: 2023/11/22 15:06:53 by momrane          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+
+char    *get_next_line(int fd);
+
+#endif
